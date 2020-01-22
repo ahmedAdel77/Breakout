728e178ae6e6c768915800b5e19c0c0108de689c
Sara Tarek <saraagamy972@gmail.com>
2020-01-22 10:06:03 +0200

    draw paddle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 /********************* Variables *********************/
 let canvas = $('#breakout')[0];
 let ctx = canvas.getContext('2d');
+ //Paddle
+const paddle_Width = 100;
+const paddle_Height = 20;
+const paddle_margin_bottom = 50;
+
+
+/********************* Objects *********************/
+
+const paddle = {
+    //x position of paddle
+    x: canvas.width / 2 - paddle_Width / 2,
+    //y position of paddle
+    y: canvas.height - paddle_Height - paddle_margin_bottom,
+    width: paddle_Width,
+    height: paddle_Height,
+    // delta x which is amount of pixels that paddle moves to the right or to left 
+    dx: 5
+}
+
+/********************* Functions *********************/
+
+// Draw Paddle
+function drawPaddle() {
+    ctx.fillStyle = "#2e3548";
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.strokeStyle = "#ffcd05";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+drawPaddle();
